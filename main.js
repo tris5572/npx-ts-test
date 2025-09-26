0c1055669b2e61f109b43c4cf61939c5ca3a2816
tris <68403260+tris5572@users.noreply.github.com>
2025-09-26 20:50:53 +0900

    chore: TypeScript に変更

--- main.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-function main() {
-  const a = 42;
-  console.log("test", a);
-}
-
-main();
---
+++ main.ts
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+const a: number = 42;
+console.log("test", a);
